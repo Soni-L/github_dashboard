ce929eb90b944deff725d74130dbbec0889cbe23
Soni Lamkaj <soni.lamkaj@sisal.al>
2025-03-22 23:03:18 +0100

    gh profile info

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useLocalStorage } from "usehooks-ts";
 
@@ -11,9 +12,37 @@ const loginWithGithub = () => {
 
 function App() {
   const [value, , removeValue] = useLocalStorage("github_access_token", 0);
+  const [userData, setUserData] = useState<object | null>(null);
+
+  useEffect(() => {
+    if (!value) {
+      setUserData(null);
+    } else {
+      fetch(`https://api.github.com/user`, {
+        headers: {
+          Authorization: `Bearer ${value}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUserData(data);
+          // Do something with the user profile data
+        })
+        .catch((error) => {
+          console.error("Error fetching user profile:", error);
+        });
+    }
+  }, [value]);
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", padding: "16px" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: "16px",
+      }}
+    >
       {!value ? (
         <button
           style={{
@@ -28,25 +57,74 @@ function App() {
           Login with Github
         </button>
       ) : (
-        <div style={{width: '100%'}}>
-          <nav style={{width: '100%', display: 'flex', justifyContent: 'flex-end'}} >
+        <>
+          <nav
+            style={{
+              width: "90%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <button
               style={{
                 backgroundColor: "red",
                 padding: "8px",
                 borderRadius: "8px",
                 cursor: "pointer",
+                minWidth: "100px",
               }}
               onClick={() => {
                 removeValue();
                 window.location.reload();
               }}
             >
-              Logout
+              Disconnect from Github
             </button>
           </nav>
-          You are logged in
-        </div>
+
+          {userData && (
+            <section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "16px",
+                border: "1px solid",
+                borderRadius: "8px",
+                gap: "8px",
+                width: "fit-content",
+                position: "relative",
+                paddingTop: "28px",
+              }}
+            >
+              <h4
+                style={{
+                  top: 0,
+                  position: "absolute",
+                  margin: "0",
+                  whiteSpace: "nowrap",
+                  left: "50%",
+                  transform: "translate(-50%, 0)",
+                  color: "gold",
+                }}
+              >
+                Github user data
+              </h4>
+              <img
+                src={userData?.avatar_url || ""}
+                style={{ height: "200px", width: "200px" }}
+              ></img>
+              <p style={{ margin: "0" }}>
+                <strong>Name:</strong> {userData?.name}
+              </p>
+              <p style={{ margin: "0" }}>
+                <strong>Email:</strong> {userData?.email}
+              </p>
+              <p style={{ margin: "0" }}>
+                <strong>Location:</strong> {userData?.location}
+              </p>
+            </section>
+          )}
+        </>
       )}
     </div>
   );
