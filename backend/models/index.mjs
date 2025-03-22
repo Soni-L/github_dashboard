e1557f407c25c9097eb503d6a74ad4c9e37106f1
Soni Lamkaj <soni.lamkaj@sisal.al>
2025-03-22 19:04:21 +0100

    changed sequelize sync location

--- backend/models/index.mjs
+++ backend/models/index.mjs
@@ -1,7 +1,4 @@
 import sequelize from '../config/database.mjs';
 import User from './user.mjs';
 
-// Initialize models
-await sequelize.sync(); // Auto creates tables if they don't exist
-
 export { sequelize, User };
--- backend/server.mjs
+++ backend/server.mjs
@@ -1,12 +1,18 @@
-import express from 'express';
-import indexRoutes from './routes/index.mjs';
-import sequelize from './config/database.mjs';
+import express from "express";
+import indexRoutes from "./routes/index.mjs";
+import sequelize from "./config/database.mjs";
 
 const app = express();
 app.use(express.json());
 
+// Sync database
+sequelize.sync().then(() => {
+  console.log("✅ Database synced.");
+});
 
-app.use('/', indexRoutes);
+app.use("/", indexRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Server running at http://localhost:${PORT}`)
+);
