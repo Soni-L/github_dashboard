5edba2237810c04300a22f4f472d54ae11ca0b24
Soni Lamkaj <soni.lamkaj@sisal.al>
2025-03-24 17:24:58 +0100

    run job if the user logged in for first time

--- backend/controllers/githubController.mjs
+++ backend/controllers/githubController.mjs
@@ -1,12 +1,27 @@
 import fetch from "node-fetch";
 import GithubToken from "../models/GithubToken.mjs";
 import StarredRepository from "../models/StarredRepository.mjs";
+import { syncStarredRepos } from "../jobs/syncStarredRepos.mjs";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+const runJobsForFirstTimeUser = async (username) => {
+  try {
+    const repoWithStats = await StarredRepository.findAll({
+      where: { username: username },
+    });
+    if (repoWithStats.length === 0) {
+      console.log("Run jobs for first time user");
+      setImmediate(syncStarredRepos);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getAccessToken = async (req, res) => {
   const { code } = req.query;
@@ -34,6 +49,10 @@ export const getAccessToken = async (req, res) => {
     });
     const userData = await userResp.json();
     const username = userData.login;
+
+    // This is for simplicity of the demo, I run the jobs here if there is no data in the database for this user
+    // this way you dont have to wait for the interval to run
+    runJobsForFirstTimeUser(username);
 
     // Save or update the token
     await GithubToken.upsert({
